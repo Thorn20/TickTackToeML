27cbc4d09b6f62a86f6085d58b3d45ff72a244d9
Thorn20 <damon.carey.10@gmail.com>
2021-09-22 13:09:37 +1000

    changed

--- TickTackToeML/Game/GameForm.cs
+++ TickTackToeML/Game/GameForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TickTackToeML.Game
@@ -100,6 +93,7 @@ namespace TickTackToeML.Game
 
         private void AiTurn()
         {
+            //to do - implement AI turn
             NextTurn();
         }
 
